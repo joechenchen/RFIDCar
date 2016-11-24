255ff87f28cabae2181cca0bb6f928b43011291a
joechenchen <599033767@qq.com>
2016-11-24 10:42:34 +0800

    ultrasonic

---
+++ ultrasonicTag/ultrasonic_prj/Objects/radio_config.d
@@ -0,0 +1,13 @@
+.\objects\radio_config.o: ..\my_drivers\radio_config.c
+.\objects\radio_config.o: ..\my_drivers\radio_config.h
+.\objects\radio_config.o: c:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.7.1\Device\Include\nrf.h
+.\objects\radio_config.o: c:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.7.1\Device\Include\nrf51.h
+.\objects\radio_config.o: c:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cm0.h
+.\objects\radio_config.o: c:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\radio_config.o: c:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmInstr.h
+.\objects\radio_config.o: c:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\cmsis_armcc.h
+.\objects\radio_config.o: c:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmFunc.h
+.\objects\radio_config.o: c:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.7.1\Device\Include\system_nrf51.h
+.\objects\radio_config.o: c:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.7.1\Device\Include\nrf51_bitfields.h
+.\objects\radio_config.o: c:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.7.1\Device\Include\nrf51_deprecated.h
+.\objects\radio_config.o: c:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.7.1\Device\Include\compiler_abstraction.h
---
+++ ultrasonicTag/ultrasonic_prj/Objects/sys.d
@@ -0,0 +1,13 @@
+.\objects\sys.o: ..\Application\sys\sys.c
+.\objects\sys.o: ..\Application\sys\sys.h
+.\objects\sys.o: c:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.7.1\Device\Include\nrf.h
+.\objects\sys.o: c:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.7.1\Device\Include\nrf51.h
+.\objects\sys.o: c:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cm0.h
+.\objects\sys.o: c:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\sys.o: c:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmInstr.h
+.\objects\sys.o: c:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\cmsis_armcc.h
+.\objects\sys.o: c:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmFunc.h
+.\objects\sys.o: c:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.7.1\Device\Include\system_nrf51.h
+.\objects\sys.o: c:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.7.1\Device\Include\nrf51_bitfields.h
+.\objects\sys.o: c:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.7.1\Device\Include\nrf51_deprecated.h
+.\objects\sys.o: c:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.7.1\Device\Include\compiler_abstraction.h
